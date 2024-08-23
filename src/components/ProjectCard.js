262fb94b55010308393b3cc40079c94abece996f
Elena Kroupkin <pocoat@gmail.com>
2024-08-23 18:20:27 +1200

    add 3 refs

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,14 +2,83 @@ import { Col } from "react-bootstrap"
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
-   <Col sm={6} md={4} lg={4}>
-   <div className="proj-imgbx">
-   <img src={imgUrl} alt="img"/>  
-    <div className="proj-txtx">
-    <h4>{title}</h4>
-    <span>{description}</span>
-    </div>
-   </div>
-   </Col>
+    <Col sm={6} md={4} lg={4}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} alt="img" />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+          {title === "GitHub icon" && (
+            <div>
+              <a
+                href="https://iconscout.com/icons/github"
+                className="text-underline font-size-sm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+              by
+              <a
+                href="https://iconscout.com/contributors/alfredo-hernandez"
+                className="text-underline font-size-sm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Icons by Alfredo Hernandez
+              </a>
+            </div>
+          )}
+          {title === "Buttons" && (
+            <div>
+              <a
+                href="https://codepen.io/Gogh/pen/GRPoqQW"
+                className="text-underline font-size-sm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+            
+              Modern Button Styles - 45 CSS Only Buttons
+              </a>
+              by
+              <a
+                href="https://iconscout.com/contributors/alfredo-hernandez"
+                className="text-underline font-size-sm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              
+Vincent Van Goggles
+              </a>
+            </div>
+          )}
+          {title === "Project structure" && (
+            <div>
+              <a
+                href="https://github.com/judygab/web-dev-projects/tree/main/personal-portfolio"
+                className="text-underline font-size-sm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+            
+              Modern Button Styles - 45 CSS Only Buttons
+              </a>
+              by
+              <a
+                href="https://github.com/judygab"
+                className="text-underline font-size-sm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+              
+           
+              Yossijaki
+               
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+    </Col>
   );
-}
+};
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import ghicon from "../assets/img/github.svg";
+import buttons45 from "../assets/img/45buttons.jpg";
+import projref from "../assets/img/projref.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import bgImg1 from "../assets/img/banner-bg1.jpg";
 import { Col, Container, Row, Nav, Tab } from "react-bootstrap";
@@ -14,12 +17,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export const Projects = () => {
-  const pdfUrls = [
-    "/AmazonQuickSightProject.pdf",
-    0,
 
-    // ... more PDF URLs
-  ];
   const projects = [
     {
       title: "Project 1",
@@ -52,34 +50,34 @@ export const Projects = () => {
       imgUrl: projImg1,
     },
   ];
-  const awsDocs = [
+  const refs = [
     {
-      title: "Project 1",
-      description: "This is a description of project 1",
-      imgUrl: projImg1,
+      title: "GitHub icon",
+      description: "Source of the Icon",
+      imgUrl: ghicon,
     },
     {
-      title: "Project 2",
+      title: "Buttons",
       description: "This is a description of project 2",
-      imgUrl: projImg2,
+      imgUrl: buttons45,
     },
     {
-      title: "Project 3",
+      title: "Project structure",
       description: "This is a description of project 3",
-      imgUrl: projImg3,
+      imgUrl: projref,
     },
     {
-      title: "Project 4",
+      title: "Project 40",
       description: "This is a description of project 4",
       imgUrl: projImg1,
     },
     {
-      title: "Project 5",
+      title: "Project 50",
       description: "This is a description of project 5",
       imgUrl: projImg1,
     },
     {
-      title: "Project 6",
+      title: "Project 60",
       description: "This is a description of project 6",
       imgUrl: projImg1,
     },
@@ -126,10 +124,10 @@ export const Projects = () => {
                         <Nav.Link eventKey="first">WEB DEV</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                        <Nav.Link eventKey="second">AWS</Nav.Link>
                       </Nav.Item>
                       <Nav.Item>
-                        <Nav.Link eventKey="third">AWS</Nav.Link>
+                        <Nav.Link eventKey="third">Appreciation</Nav.Link>
                       </Nav.Item>
                     </Nav>
 
@@ -139,6 +137,7 @@ export const Projects = () => {
                         isVisible ? "animate__animated animate__slideInUp" : ""
                       }
                     >
+                      {/* ======  1st tab ======  */}
                       <Tab.Pane eventKey="first">
                         <Row>
                           {projects.map((project, index) => {
@@ -146,71 +145,65 @@ export const Projects = () => {
                           })}
                         </Row>
                       </Tab.Pane>
-
+                      {/* ======  2nd tab ======  */}
                       <Tab.Pane eventKey="second">
+                        <div className="pdf-container">
+                          <div className="border-container">
+                            <div className="pdf-buttons-container">
+                              {pdfFiles.map((pdf, index) => (
+                                <a
+                                  key={index}
+                                  href="#_"
+                                  className="pdf-button relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
+                                  onClick={() => setSelectedPdf(pdf.file)}
+                                >
+                                  <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                                    <svg
+                                      className="w-6 h-6"
+                                      fill="none"
+                                      stroke="currentColor"
+                                      viewBox="0 0 24 24"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                      <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                      ></path>
+                                    </svg>
+                                  </span>
+                                  <span className="absolute flex items-center justify-center w-full h-full  transition-all duration-300 transform group-hover:translate-x-full ease">
+                                    {pdf.name}
+                                  </span>
+                                  <span className="relative invisible">
+                                    Button
+                                  </span>
+                                </a>
+                              ))}
+                            </div>
+                          </div>
+                          <PdfViewer file={selectedPdf} />
+                        </div>
+                      </Tab.Pane>
+
+                      {/* ======  3rd tab ======  */}
+
+                      <Tab.Pane eventKey="third">
                         <Row>
-                          awsDocs Lorem ipsum dolor sit amet consectetur
-                          adipisicing elit. Cumque quam, quod neque provident
-                          velit, rem explicabo excepturi id illo molestiae
-                          blanditiis, eligendi dicta officiis asperiores
-                          delectus quasi inventore debitis quo.
-                          {awsDocs.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
+                          <p>
+                            projects I used as a reference to build my projects
+                          </p>
+                          <br/>
+                          {refs.map((ref, index) => {
+                            return <ProjectCard key={index} {...ref} />; 
                           })}
                         </Row>
                       </Tab.Pane>
-
-<Tab.Pane eventKey="third">
-  <div className="pdf-container">
-    <div className="border-container">
-
-
-      <div className="pdf-buttons-container">
-        {pdfFiles.map((pdf, index) => (
-          <a
-            key={index}
-            href="#_"
-            className="pdf-button relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
-            onClick={() => setSelectedPdf(pdf.file)}
-          >
-            <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                ></path>
-              </svg>
-            </span>
-            <span className="absolute flex items-center justify-center w-full h-full  transition-all duration-300 transform group-hover:translate-x-full ease">
-              {pdf.name}
-            </span>
-            <span className="relative invisible">Button</span>
-          </a>
-        ))}
-      </div>
-    </div>
-    <PdfViewer file={selectedPdf} />
-  </div>
-</Tab.Pane>
-
-
-
-
-
-
                     </Tab.Content>
                   </Tab.Container>
                 </div>
               )}
-              
             </TrackVisibility>
           </Col>
         </Row>
