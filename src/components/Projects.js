96528b734fedefbdf9a8699f6ef8eefff0cedfb1
Elena Kroupkin <pocoat@gmail.com>
2024-08-23 14:58:33 +1200

    purple button for PDF docs WIP

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,10 @@ import "animate.css";
 import TrackVisibility from "react-on-screen";
 import React, { useState } from "react";
 import PdfViewer from "./PdfViewer";
-import "../projects-styles.css"
+import "../projects-styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
 export const Projects = () => {
   const pdfUrls = [
     "/AmazonQuickSightProject.pdf",
@@ -105,9 +108,6 @@ export const Projects = () => {
                   }
                 >
                   <h2>Projects</h2>
-                
-                 
-
 
                   <p>
                     Lorem Ipsum is simply dummy text of the printing and
@@ -160,36 +160,72 @@ export const Projects = () => {
                         </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
-                      
-                      <div class="border w-full h-40 flex items-center justify-center">
-                      <a href="#_" class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group">
-                        <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-                          </svg>
-                        </span>
-                        <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">Button 18</span>
-                        <span class="relative invisible">Button </span>
-                      </a>
-                    </div>
+{ /*  =====================   purple button  =======================  */}
 
-                      {activeTab === 'third' && (
+
+                        <div class="border w-full h-40 flex items-center justify-center">
+                          <a
+                            href="#_"
+                            class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group"
+                          >
+                          { /*  ======  hover  ========  */}                           
+
+                            <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
+                             
+                              <svg
+                                class="w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  stroke-width="2"
+                                  d="M14 5l7 7m0 0l-7 7m7-7H3"
+                                ></path>
+                              </svg>
+                            </span>
+
+                            
+                            <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
+                            VVVVVVVV
+                            </span>
+                            
+                            <span class="relative invisible">Button </span>
+                          </a>
+                        </div>
+
+
+{ /*  =====================  PDFs tytles   =======================  */}    
+
+                        {activeTab === "third" && (
                           <div>
-                              <Nav variant="pills" className="nav-pills mb-3 justify-center align-items-center">
-                                  {pdfFiles.map((pdf, index) => (
-                                      <Nav.Item key={index}>
-                                          <Nav.Link onClick={() => setSelectedPdf(pdf.file)}>{pdf.name}</Nav.Link>
-                                      </Nav.Item>
-                                  ))}
-                              </Nav>
-                              <PdfViewer file={selectedPdf} />
+                            <Nav
+                              variant="pills"
+                              className="nav-pills mb-3 justify-center align-items-center"
+                            >
+                              {pdfFiles.map((pdf, index) => (
+                                <Nav.Item key={index}>
+                                <Nav.Link className="pdf-button" onClick={() => setSelectedPdf(pdf.file)}>
+                                <span className="pdf-name absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">{pdf.name}</span>
+                              
+                              </Nav.Link>                                </Nav.Item>
+                              ))}
+                            </Nav>
+                            <PdfViewer file={selectedPdf} />
                           </div>
-                      )}
-                  </Tab.Pane>
+                        )}
+
+
+
+                      </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
                 </div>
               )}
+              
             </TrackVisibility>
           </Col>
         </Row>
